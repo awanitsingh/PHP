ad2c495caf1b5e0c06b18cba8bff11b3b8d418c0
Awanit Kumar Singh <awanitsingh8873@gmail.com>
2024-09-04 11:37:48 +0530

    Functions

--- Static_Var.php
+++ Static_Var.php
@@ -31,3 +31,60 @@ Static_var();
 Static_var();
 
 ?>
+
+
+<?php
+function start($string) {
+    if($string<45) 
+    return 20;
+else 
+return 40;
+}
+
+$t = start(90);
+if($t<20) {
+    echo"Have a good morning";
+}
+else {
+    echo"Have a good night";
+}
+?>
+
+
+
+
+<?php
+function TV($string) {
+    echo "my fav TV show is ".$string;
+    function b() {
+        echo "I am here to spoil this code";
+    }
+}
+function b() {
+    echo"I am here to spoil this code";
+}
+b();
+TV("Sherlock");
+?>
+
+
+
+<?php
+function CallAll($x,$y) {
+    echo($x+$y);
+    echo"<br>";
+    echo($x-$y);
+    echo"<br>";
+    echo($x*$y);
+    echo"<br>";
+    echo($x/$y);
+    echo"<br>";
+    echo($x%$y);
+    echo"<br>";
+    
+}
+
+$x = 10;
+$y = 6;
+CallAll();
+?>
