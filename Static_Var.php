50cc0c2e8b13422e9d8d11338e7b83c27329f6e0
Awanit Kumar Singh <awanitsingh8873@gmail.com>
2024-09-04 10:51:43 +0530

    Static Variable

--- Static_Var.php
+++ Static_Var.php
@@ -1,11 +1,33 @@
+<!-- <?php
+// function myTest() {
+//     static $x = 1;
+//     echo $x;
+//     $x++;
+// }
+// myTest();
+// echo"<br>";
+// myTest();
+// echo"<br>";
+?> -->
+
+
+
 <?php
-function myTest() {
-    static $x = 1;
-    echo $x;
-    $x++;
+function Static_var()
+{
+    static $num1=2;
+    $num2=5;
+    $num1++;
+    $num2++;
+
+    echo "Static: ".$num1."<br>";
+    echo "NonStatic: ".$num2."<br>";
 }
-myTest();
-echo"<br>";
-myTest();
-echo"<br>";
+Static_var();
+Static_var();
+Static_var();
+Static_var();
+Static_var();
+Static_var();
+
 ?>
